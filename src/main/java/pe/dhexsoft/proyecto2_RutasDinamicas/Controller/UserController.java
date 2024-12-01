d214ef0e2f56d7f2fec48d95e1f133ba3915e9fe
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-30 22:41:40 -0500

    pathvariable

--- src/main/java/pe/dhexsoft/proyecto2_RutasDinamicas/Controller/UserController.java
+++ src/main/java/pe/dhexsoft/proyecto2_RutasDinamicas/Controller/UserController.java
@@ -5,4 +5,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/user")
+public class UserController {
 
+    @GetMapping("/{name}/age/{id}") //lo de parentisis{} es variables
+    public String greetUser(@PathVariable String name, @PathVariable int age){ //para cada paremetro tiene que tener su @PathVariable
+        return "hola usuario " + name + " tu edad es " + age;
+    }
+
+
+}
